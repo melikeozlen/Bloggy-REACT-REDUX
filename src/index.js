92fdfb215873bb6dfcaeaf0b57b25e8665f0f94c
Melike ÖZLEN <44196940+melikeozlen@users.noreply.github.com>
2022-05-27 00:13:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ store.subscribe(() => {
 })
 
 //BLOG EKLEME
-const blog1 = store.dispatch(addBlog({ title: 'Caesar Deviled Eggs', 
-description: 'This is a really fun take on classic deviled eggs, it’s also delightfully simple.Friends coming over? Caesar the day and whip up a batch of these' }));
-const blog2 = store.dispatch(addBlog({ title: 'Listen to kwes e’s “crocodile tears”: The Ones', description: '“crocodile tears” by kwes e, a 21-year-old rapper from Canterbury, England has all the elements of a Drain Gang hit: dissociative vocals, fractal drums, and deep-felt melancholy. To impress the object of his affection, kwes e talks about obtaining fancy clothes–a Prada suit is mentioned along with the Lacoste gear that gives the song its winking title. kwes e gives the sad boy warble his own spin, blending the hopeful brags (“I just got some money it’s for me and it’s for only you”) with comic asides (“Were you loving me because I got designer on my crotch?”) in an altered falsetto. By the end of the song, his voice is drowned in static, and everything goes quiet.' }));
-const blog3 = store.dispatch(addBlog({ title: 'Blog title 3', description: 'Blog description 3', dateAdded: Date.now() }));
-store.dispatch(addBlog({ title: 'Blog title 3', description: 'Blog description 3', dateAdded: Date.now() }));
+const blog1 = store.dispatch(addBlog({ title: 'Backend Developer', 
+description: 'Backend developers build code that allows a database and an application to communicate with one another. Backend developers take care and maintain the back-end of a website, Including databases, servers, and apps, and they control what you dont see' }));
+const blog2 = store.dispatch(addBlog({ title: 'Redux - TOOLKIT', description: `The Redux Toolkit package is intended to be the standard way to write Redux logic. It was originally created to help address three common concerns about Redux:
 
+"Configuring a Redux store is too complicated"
+"I have to add a lot of packages to get Redux to do anything useful"
+"R
 //BLOG SİLME
 //store.dispatch(removeBlog({ id: blog2.blog.id }))
 
